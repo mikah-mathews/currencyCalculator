47a0a5bc212a128db90fdd8f6acec6dc96c3170f
Mikah Mathews <mikah@outofband.org>
2022-02-11 09:38:09 -0800

    Add getCurrency method

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,5 +1,13 @@
 export default class Currency {
-  async getCurrency() {
-
+  static async getCurrency() {
+    try {
+      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      if (!response.ok) {
+        throw Error(response['error-type']);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
