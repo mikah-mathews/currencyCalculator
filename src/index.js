5ceae98e394d6d100c7f64485ba8d74e5f75b5e8
Mikah Mathews <mikah@outofband.org>
2022-02-11 14:53:30 -0800

    WIP: researching weird error message

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './js/currency.js';
 
+function getElements(response) {
+  if (response) {
+ 
+  } else {
 
+  }
+}
+
+async function callCurrencyApi() {
+  const response = await Currency.getCurrencyData();
+
+}
 
 
 $(function() {
