c154a24aabdb43eeeda9a9c6d65c9fe30cf5f282
Mikah Mathews <mikah@outofband.org>
2022-02-11 13:33:29 -0800

    BUGFIX: Input now will correctly get input

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './js/currency.js';
 
+
+
+
 $(function() {
   $('#submit').on('click', function() {
-    let conversionAmount = $('#conversionAmount').val();
+    let conversionAmount = $('#conversion-amount').val();
     let conversionCurrency = $("input:radio[name=currency]:checked").val();
     alert(`This is the conversion amount ${conversionAmount} and this is the currency selected ${conversionCurrency}`);
   });
